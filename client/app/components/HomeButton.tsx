79b155e74f05cbc559031444ea40edf6faebef30
kseyo <kseyo@veepee.com>
2024-07-18 12:41:06 +0200

    feat(client):to responsive

--- client/app/components/HomeButton.tsx
+++ client/app/components/HomeButton.tsx
@@ -17,10 +17,10 @@ export default function HomeButton({ children, value }: any) {
   return (
     <Link
       href="#Home"
-      className="flex items-center px-2 border rounded-lg text-sm font-medium bg-gray-200 hover:bg-gray-300 text-gray-800 border-gray-300 hover:-translate-y-1 duration-300 cache"
+      className="flex items-center px-2 border rounded .rounded-lg text-sm font-medium bg-gray-200 hover:bg-gray-300 text-gray-800 border-gray-300 hover:-translate-y-1 duration-300 cache"
     >
       {children}
-      <p>{value}</p>
+      <p id="Home">{value}</p>
     </Link>
   );
 }
--- client/app/components/Menu.tsx
+++ client/app/components/Menu.tsx
@@ -1,6 +1,100 @@
+// "use client";
+
+// import { JSX, SVGProps, useState } from "react";
+// import Link from "next/link";
+// import { Plus } from "lucide-react";
+
+// export default function Menu() {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
+//   const closeMenu = () => {
+//     setIsOpen(false);
+//   };
+//   return (
+//     <div className="md:z-10 sm:z-10">
+//       <button
+//         className="p-2 rounded-md hover:bg-muted focus:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"
+//         onClick={toggleMenu}
+//       >
+//         {isOpen ? (
+//           <Plus className="rotate-45" />
+//         ) : (
+//           <MenuIcon className="h-6 w-6" />
+//         )}
+//       </button>
+//       {isOpen && (
+//         <div
+//           className="w-56 rounded-md bg-background shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+//           role="menu"
+//           aria-orientation="vertical"
+//           aria-labelledby="menu-button"
+//         >
+//           <div className="py-1" role="none">
+//             <Link
+//               href="#Home"
+//               className="block px-4 py-2 text-sm rounded .rounded-lg hover:bg-gray-200"
+//             >
+//               Home
+//             </Link>
+//             <Link
+//               href="#MyVeepeeExperience"
+//               className="block px-4 py-2 text-sm rounded .rounded-lg hover:bg-gray-200"
+//               role="menuitem"
+//             >
+//               My Veepee experience
+//             </Link>
+//             <Link
+//               href="#SomeProject"
+//               className="block px-4 py-2 text-sm rounded .rounded-lg hover:bg-gray-200"
+//               role="menuitem"
+//             >
+//               Some projects
+//             </Link>
+//             <Link
+//               href="#CurrentStack"
+//               className="block px-4 py-2 text-sm rounded .rounded-lg hover:bg-gray-200"
+//             >
+//               Current Stack
+//             </Link>
+//             <div className="block px-4 py-2 text-sm rounded .rounded-lg hover:bg-gray-200">
+  
+//               <a href="/KillianCv.pdf" download="KillianCv.pdf">CV</a>
+
+//             </div>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// function MenuIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+//   return (
+//     <svg
+//       {...props}
+//       xmlns="http://www.w3.org/2000/svg"
+//       width="24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//       fill="none"
+//       stroke="currentColor"
+//       strokeWidth="2"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//     >
+//       <line x1="4" x2="20" y1="12" y2="12" />
+//       <line x1="4" x2="20" y1="6" y2="6" />
+//       <line x1="4" x2="20" y1="18" y2="18" />
+//     </svg>
+//   );
+// }
+
 "use client";
 
-import { JSX, SVGProps, useState } from "react";
+import { SVGProps, useState } from "react";
 import Link from "next/link";
 import { Plus } from "lucide-react";
 
@@ -10,13 +104,15 @@ export default function Menu() {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
   const closeMenu = () => {
     setIsOpen(false);
   };
+
   return (
-    <div className="md:z-10 sm:z-10">
+    <div className="relative z-10">
       <button
-        className="p-2 rounded-md hover:bg-muted focus:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"
+        className="p-2 rounded-md hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"
         onClick={toggleMenu}
       >
         {isOpen ? (
@@ -27,7 +123,7 @@ export default function Menu() {
       </button>
       {isOpen && (
         <div
-          className="w-56 rounded-md bg-background shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+          className="absolute mt-2 w-56 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
           role="menu"
           aria-orientation="vertical"
           aria-labelledby="menu-button"
@@ -35,34 +131,34 @@ export default function Menu() {
           <div className="py-1" role="none">
             <Link
               href="#Home"
-              className="block px-4 py-2 text-sm rounded .rounded-lg hover:bg-gray-200"
+              className="block px-4 py-2 text-sm rounded-lg hover:bg-gray-200"
             >
               Home
             </Link>
             <Link
               href="#MyVeepeeExperience"
-              className="block px-4 py-2 text-sm rounded .rounded-lg hover:bg-gray-200"
+              className="block px-4 py-2 text-sm rounded-lg hover:bg-gray-200"
               role="menuitem"
             >
               My Veepee experience
             </Link>
             <Link
               href="#SomeProject"
-              className="block px-4 py-2 text-sm rounded .rounded-lg hover:bg-gray-200"
+              className="block px-4 py-2 text-sm rounded-lg hover:bg-gray-200"
               role="menuitem"
             >
               Some projects
             </Link>
             <Link
               href="#CurrentStack"
-              className="block px-4 py-2 text-sm rounded .rounded-lg hover:bg-gray-200"
+              className="block px-4 py-2 text-sm rounded-lg hover:bg-gray-200"
             >
               Current Stack
             </Link>
-            <div className="block px-4 py-2 text-sm rounded .rounded-lg hover:bg-gray-200">
-  
-              <a href="/KillianCv.pdf" download="KillianCv.pdf">CV</a>
-
+            <div className="block px-4 py-2 text-sm rounded-lg hover:bg-gray-200">
+              <a href="/KillianCv.pdf" download="KillianCv.pdf">
+                CV
+              </a>
             </div>
           </div>
         </div>
@@ -85,9 +181,9 @@ function MenuIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <line x1="4" x2="20" y1="12" y2="12" />
-      <line x1="4" x2="20" y1="6" y2="6" />
-      <line x1="4" x2="20" y1="18" y2="18" />
+      <line x1="4" y1="12" x2="20" y2="12" />
+      <line x1="4" y1="6" x2="20" y2="6" />
+      <line x1="4" y1="18" x2="20" y2="18" />
     </svg>
   );
 }
